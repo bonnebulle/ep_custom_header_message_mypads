fd27cf4531fdd6222fbe7021987db672da769c44
Samir Sayyad <samir.saiad@gmail.com>
2020-07-06 18:35:17 +0430

    tested ok

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,15 @@
 
 exports.aceInitialized = function(hook, context){
     clearTimeout(timer);
+
     var timer = setTimeout(function(){
         $("body").prepend("<div class='ep_custom_header' id='ep_custom_header'> <p class='ep_custom_header_text'> "+
-        clientVars.ep_custom_header_message.message+" </p> <div id='ep_custom_header_close'>X</div></div>")
+        clientVars.ep_custom_header_message.message+" </p> <div id='ep_custom_header_close'>&times;</div></div>")
+        $("#ep_custom_header_close").bind('click', function(e){
+            console.log('click');
+            $("#ep_custom_header").fadeOut()
+            
+        });
     },120000);
    
 }
