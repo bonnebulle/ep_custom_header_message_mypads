e3b8854daafbbbbeb48a282d3d989a819269976f
Samir Sayyad <samir.saiad@gmail.com>
2020-07-06 16:38:47 +0430

    close sign

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,7 @@ exports.aceInitialized = function(hook, context){
     clearTimeout(timer);
     var timer = setTimeout(function(){
         $("body").prepend("<div class='ep_custom_header' id='ep_custom_header'> <p class='ep_custom_header_text'> "+
-        clientVars.ep_custom_header_message.message+" </p> </div>")
+        clientVars.ep_custom_header_message.message+" </p> <div id='ep_custom_header_close'>X</div></div>")
     },120000);
    
 }
