5813ad56698298255555fcc869c211093a0204c7
Samir Sayyad <samir.saiad@gmail.com>
2021-01-15 15:52:03 +0330

    for mobile view

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,21 @@
 
 exports.aceInitialized = function(hook, context){
-    clearTimeout(timer);
 
-    var timer = setTimeout(function(){
-        $("body").prepend("<div class='ep_custom_header' id='ep_custom_header'> <p class='ep_custom_header_text'> "+
-        clientVars.ep_custom_header_message.message+" </p> <div id='ep_custom_header_close'>&times;</div></div>")
-        $("#ep_custom_header_close").bind('click', function(e){
-            console.log('click');
-            $("#ep_custom_header").fadeOut()
-            $("#wrtc_modal").css({"top" : 0})
+    if (!window.matchMedia('(max-width: 720px)').matches) {
+        clearTimeout(timer);
 
-        });
-        $("#wrtc_modal").css({"top" : '4%'})
-    },300000);
+        var timer = setTimeout(function(){
+            $("body").prepend("<div class='ep_custom_header' id='ep_custom_header'> <p class='ep_custom_header_text'> "+
+            clientVars.ep_custom_header_message.message+" </p> <div id='ep_custom_header_close'>&times;</div></div>")
+            $("#ep_custom_header_close").bind('click', function(e){
+                console.log('click');
+                $("#ep_custom_header").fadeOut()
+                $("#wrtc_modal").css({"top" : 0})
+
+            });
+            $("#wrtc_modal").css({"top" : '4%'})
+        },300000);
+    }
    
 }
 
